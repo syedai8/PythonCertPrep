262c892ed59e6debcf52f322a20b4a1a6e6c6e76
syedai8 <syeda.islam275@gmail.com>
2025-05-14 09:24:18 -0400

    Update 121-conversion.py

--- Domain1/Student/121-conversion.py
+++ Domain1/Student/121-conversion.py
@@ -1,4 +1,4 @@
-# rating is supposed to be a whole number
-rating = (input("Enter a rating between 1 and 5"))
-points = (rating) * 100
+#rating is meant to be a whole number
+rating = (input("Enter a rating between 1 and 5")) #input variable stores strig data type be default
+points = (rating) * 100 #since ratng is a string, this writes that string 100 times 
 print("You have " + (points) + " to start.")
